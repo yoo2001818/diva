e484b531fdbce98b57c362e8fe3bea537de40c81
Duknam Yoo <yoo2001818@gmail.com>
2024-10-09 22:30:26 +0900

    Add Box

---
+++ src/layout/Box.ts
@@ -0,0 +1,23 @@
+export class Box {
+  top: number = 0;
+  left: number = 0;
+  width: number = 0;
+  height: number = 0;
+}
+
+export class LayoutBox {
+  marginTop: number = 0;
+  marginRight: number = 0;
+  marginBottom: number = 0;
+  marginLeft: number = 0;
+  borderTop: number = 0;
+  borderRight: number = 0;
+  borderBottom: number = 0;
+  borderLeft: number = 0;
+  paddingTop: number = 0;
+  paddingRight: number = 0;
+  paddingBottom: number = 0;
+  paddingLeft: number = 0;
+  width: number = 0;
+  height: number = 0;
+}
