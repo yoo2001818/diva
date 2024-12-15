ac66e95dce87e2786989bb1bccc65d30afb64a11
Duknam Yoo <yoo2001818@gmail.com>
2024-12-15 22:46:03 +0900

    Add main entrypoint

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-// TODO
+import { Document } from './dom/Document';
+import { CanvasRenderer } from './render/CanvasRenderer';
+
+const doc = new Document();
+const canvasEl = document.createElement('canvas');
+canvasEl.width = 1024;
+canvasEl.height = 768;
+document.body.appendChild(canvasEl);
+const renderer = new CanvasRenderer(doc, canvasEl);
+renderer.layout();
+renderer.render();
