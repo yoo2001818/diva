2652565d83f615d5a9be18e5c8b2b6b9617ca02c
Duknam Yoo <yoo2001818@gmail.com>
2025-01-11 23:23:36 +0900

    Update FormattingContext

--- src/layout/FormattingContext.ts
+++ src/layout/FormattingContext.ts
@@ -130,4 +130,15 @@ export function layoutInlines(
   // placed under block. Currently the model doesn't have a structure to
   // represent that; Perhaps the StyleData could define "virtualParent" property
   // or something like that.
+  // Let's assume that the parent is the container, and plan out what to do:
+  // 1. Initially, it would need to check what elements (and text) reside in
+  //    the same line. In other words, each bounds of each node is determined
+  //    and "chopped up" to each lines. This must include all the children,
+  //    including grandchildren to calculate it. In other words, just like how
+  //    a text render would do (it is!) It must traverse the node in-order and
+  //    lay out the text and the nodes.
+  // 2. After a line is finished, the vertical height of the line will be
+  //    calculated. Text baseline and other heights are calculated using the
+  //    font, and other nodes are lined up to match that. After the height is
+  //    calculated, the nodes are placed according to the line.
 }
