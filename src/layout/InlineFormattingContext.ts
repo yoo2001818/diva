31f5b9bffba25c47b52cc319903b43410770aedc
Duknam Yoo <yoo2001818@gmail.com>
2025-01-23 22:35:54 +0900

    Add line feeding

--- src/layout/InlineFormattingContext.ts
+++ src/layout/InlineFormattingContext.ts
@@ -4,9 +4,11 @@ import { StyleData } from './StyleData';
 export class InlineFormattingContext {
   containingBox: Box;
   lineBoxes: LayoutBox[];
+  lines: { box: Box; items: StyleData[]; remaining: number }[];
   constructor(containingBox: Box) {
     this.containingBox = containingBox;
     this.lineBoxes = [];
+    this.lines = [];
   }
   feed(item: StyleData): void {
     // 1. Initially, it would need to check what elements (and text) reside in
@@ -19,7 +21,25 @@ export class InlineFormattingContext {
     //    calculated. Text baseline and other heights are calculated using the
     //    font, and other nodes are lined up to match that. After the height is
     //    calculated, the nodes are placed according to the line.
+    const lineBox = new Box();
+    lineBox.width = this.containingBox.width;
+    // FIXME: inline-in-inline nodes must be dealt differently
+    item.layout(lineBox);
+    const box = item.principalBox;
+    let currentLine = this.lines[this.lines.length - 1];
+    if (currentLine == null || currentLine.remaining < box.clientWidth) {
+      this.newline();
+      currentLine = this.lines[this.lines.length - 1];
+    }
+    currentLine.items.push(item);
+    currentLine.remaining -= box.clientWidth;
   }
-  newline(): void {}
+  newline(): void {
+    this.lines.push({
+      box: new Box(),
+      items: [],
+      remaining: this.containingBox.width,
+    });
+  }
   finalize(): void {}
 }
