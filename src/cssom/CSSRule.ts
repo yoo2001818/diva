5064244a167ad62b3b32af137b9b4bd17e77e78b
Duknam Yoo <yoo2001818@gmail.com>
2025-02-22 22:02:41 +0900

    Implement CSSStyleSheet methods

--- src/cssom/CSSRule.ts
+++ src/cssom/CSSRule.ts
@@ -2,18 +2,36 @@ import { CSSStyleDeclaration } from './CSSStyleDeclaration';
 import { CSSStyleSheet } from './CSSStyleSheet';
 
 export class CSSRule {
-  cssText: string = '';
+  _cssText: string = '';
+  _parentStyleSheet: CSSStyleSheet | null = null;
+
+  constructor(cssText: string) {
+    this._cssText = cssText;
+  }
 
   get parentRule(): CSSRule | null {
     return null;
   }
 
   get parentStyleSheet(): CSSStyleSheet | null {
-    return null;
+    return this._parentStyleSheet;
+  }
+
+  get cssText(): string {
+    return this._cssText;
+  }
+
+  set cssText(_value: string) {
+    // Must do nothing
   }
 }
 
 export class CSSStyleRule extends CSSRule {
   selectorText: string = '';
   style: CSSStyleDeclaration = new CSSStyleDeclaration();
+}
+
+export function parseCSSRules(text: string): CSSRule[] {
+  // TODO: Add a CSS parser, construct them, etc
+  return [];
 }
--- src/cssom/CSSStyleSheet.ts
+++ src/cssom/CSSStyleSheet.ts
@@ -1,3 +1,4 @@
+import { parseCSSRules } from './CSSRule';
 import { CSSRuleList } from './CSSRuleList';
 
 export class CSSStyleSheet {
@@ -9,15 +10,32 @@ export class CSSStyleSheet {
     return null;
   }
 
-  insertRule(rule: string, index: number = 0): number {
-    return 0;
+  insertRule(text: string, index: number = 0): number {
+    if (index > this.cssRules.length) {
+      throw new DOMException('IndexSizeError');
+    }
+    const rules = parseCSSRules(text);
+    this.cssRules.splice(index, 0, ...rules);
+    rules.forEach((rule) => (rule._parentStyleSheet = this));
+    return index;
   }
 
-  deleteRule(index: number): void {}
+  deleteRule(index: number): void {
+    if (index >= this.cssRules.length) {
+      throw new DOMException('IndexSizeError');
+    }
+    this.cssRules[index]._parentStyleSheet = null;
+    this.cssRules.splice(index, 1);
+  }
 
   replace(text: string): Promise<CSSStyleSheet> {
+    this.replaceSync(text);
     return Promise.resolve(this);
   }
 
-  replaceSync(text: string): void {}
+  replaceSync(text: string): void {
+    this.cssRules.forEach((rule) => (rule._parentStyleSheet = null));
+    this.cssRules.splice(0, this.cssRules.length);
+    this.insertRule(text);
+  }
 }
