6f7c693bf0b73231288b32ca77d09d852ba950ec
Duknam Yoo <yoo2001818@gmail.com>
2025-03-15 22:25:48 +0900

    Add getSpecificity

--- src/cssom/utils.ts
+++ src/cssom/utils.ts
@@ -1,5 +1,15 @@
+import { HTMLElement } from '../html/HTMLElement';
+import { parseSelectors } from '../parser/selector';
+
 export const kebabize = (str: string) =>
   str.replace(
     /[A-Z]+(?![a-z])|[A-Z]/g,
     ($, ofs) => (ofs ? '-' : '') + $.toLowerCase(),
   );
+
+export function getSpecificity(
+  element: HTMLElement,
+  selectorText: string,
+): number {
+  const selector = parseSelectors(selectorText);
+}
